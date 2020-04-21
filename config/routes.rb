f7d46b3f08078fdf6a9aa9ca2d0db5b10dfe079f
Mirshad Oz <mirshadoz@Mirshads-MacBook-Pro.local>
2020-04-21 23:01:31 +0600

    Reverted back from Integrating Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
+
   resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
+
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.append("Provide", new ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Pooper: ['pooper.js', 'default']
+}))
 
 module.exports = environment
